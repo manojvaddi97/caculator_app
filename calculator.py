dd27d85f1e6853c4412fbc455220fd2b61a25e3c
manojvaddi497 <manojvaddi497@gmail.com>
2024-05-13 19:07:51 -0400

    adding divsion feature

--- calculator.py
+++ calculator.py
@@ -7,18 +7,25 @@ def subtract(x, y):
 def multiply(x, y):
     return x * y
 
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero is not allowed."
+    else:
+        return x / y
+		
 
 print("Select operation:")
 print("1. Addition")
 print("2. Subtraction")
 print("3. Multiplication")
+print("4. Division")
 
 
 
 while True:
-    choice = input("Enter choice (1/2/3): ")
+    choice = input("Enter choice (1/2/3/4): ")
 
-    if choice in ('1', '2', '3'):
+    if choice in ('1', '2', '3', '4'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
@@ -28,6 +35,8 @@ while True:
             print("Result:", subtract(num1, num2))
         elif choice == '3':
             print("Result:", multiply(num1, num2))
+        elif choice == '4':
+            print("Result:", divide(num1, num2))
         
         
         next_calculation = input("Perform another calculation? (yes/no): ")
