9030fd5239a84a4062a6364f37f8fede1ab32bd1
manojvaddi497 <manojvaddi497@gmail.com>
2024-05-13 20:00:44 -0400

    adding modulus feature

--- calculator.py
+++ calculator.py
@@ -12,20 +12,22 @@ def divide(x, y):
         return "Error! Division by zero is not allowed."
     else:
         return x / y
-		
+
+def modulus(x, y):
+    return x % y
 
 print("Select operation:")
 print("1. Addition")
 print("2. Subtraction")
 print("3. Multiplication")
 print("4. Division")
-
+print("5. Modulus")
 
 
 while True:
-    choice = input("Enter choice (1/2/3/4): ")
+    choice = input("Enter choice (1/2/3/4/5): ")
 
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '4', '5'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
@@ -37,6 +39,8 @@ while True:
             print("Result:", multiply(num1, num2))
         elif choice == '4':
             print("Result:", divide(num1, num2))
+        elif choice == '5':
+            print("Result:", modulus(num1, num2))
         
         
         next_calculation = input("Perform another calculation? (yes/no): ")
