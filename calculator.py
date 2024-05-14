e88d1d9a069f553110127ceda004825bf2a827ed
manojvaddi497 <manojvaddi497@gmail.com>
2024-05-13 20:04:43 -0400

    adding percentage feature

--- calculator.py
+++ calculator.py
@@ -12,20 +12,25 @@ def divide(x, y):
         return "Error! Division by zero is not allowed."
     else:
         return x / y
-		
+
+def modulus(x, y):
+    return x % y
+
+def percentage(x, y):
+    return (x / y) * 100
 
 print("Select operation:")
 print("1. Addition")
 print("2. Subtraction")
 print("3. Multiplication")
 print("4. Division")
-
-
+print("5. Modulus")
+print("6. Percentage")
 
 while True:
-    choice = input("Enter choice (1/2/3/4): ")
+    choice = input("Enter choice (1/2/3/4/5/6): ")
 
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '4', '5', '6'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
@@ -37,7 +42,10 @@ while True:
             print("Result:", multiply(num1, num2))
         elif choice == '4':
             print("Result:", divide(num1, num2))
-        
+        elif choice == '5':
+            print("Result:", modulus(num1, num2))
+        elif choice == '6':
+            print("Result:", percentage(num1, num2))
         
         next_calculation = input("Perform another calculation? (yes/no): ")
         if next_calculation.lower() != 'yes':
